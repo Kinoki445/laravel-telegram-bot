d121072fe6492ddeaae5fc5dd69f07ff7088abe9
Pisex <kardapolce@gmail.com>
2024-05-31 15:00:14 +0500

    Добавление обработчика даты

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -87,73 +87,66 @@ class TelegramController extends Controller
         return $bot->sendMessage($parameter);
     }
 
-    public function schedule_teacher_action(Nutgram $bot) {
-        $id= $bot->user()->id;
-        $user = User::where('id_user', $id)->first();
-        $currentGroup = $user->group;
-        if (!(is_null($currentGroup))) {
-            $import = new ApiController();
-            $response = $import->client->request('GET', '');
-            $data = (json_decode($response->getBody()));
+    public function callback_action_teacher(Nutgram $bot, $date_fromcall, $teacher) {
+        $import = new ApiController();
+        $response = $import->client->request('GET', '');
+        $data = (json_decode($response->getBody()));
 
-            $newArray = [];
-            $counter = 0;
+        $newArray = [];
+        $counter = 0;
 
-            foreach ($data as $value) {
-                if ($counter < 5) {
-                    $date = implode('-', array_reverse(explode('.', $value)));
-                    $newArray[] = $date;
-                    $counter++;
-                } else {
-                    break;
-                }
+        foreach ($data as $value) {
+            if ($counter < 5) {
+                $date = implode('-', array_reverse(explode('.', $value)));
+                $newArray[] = $date;
+                $counter++;
+            } else {
+                break;
             }
-
-            $response = $import->client->request('GET', "$newArray[0]/group/$currentGroup");
-            $schedule = (json_decode($response->getBody()->getContents(), true));
-
-            $result[] = "Расписание на $newArray[0]\nГруппы $currentGroup";
-            foreach ($schedule['schedule'] as $item) {
-                // Обработка каждого элемента расписания
-                if (isset($item['lesson'], $item['name'], $item['teachers'], $item['rooms'])) {
-                    $lesson = $item['lesson'];
-                    $name = str_replace("\n", ' ', $item['name']);
-                    $teachers = implode(', ', $item['teachers']); // Преобразование массива учителей в строку
-                    $rooms = implode(', ', $item['rooms']); // Преобразование массива комнат в строку
-
-                    // Создание строки с информацией
-                    $scheduleString = "\nУрок: $lesson\nНазвание: $name\nПреподаватель: $teachers - $rooms";
-
-                    // Добавление строки в результат
-                    $result[] = $scheduleString;
-                } else {
-                    $result[] = "Invalid schedule item structure.";
-                }
-            }
-
-            // Создаем объект разметки клавиатуры
-            $keyboard = InlineKeyboardMarkup::make();
-
-            // Добавляем кнопки по одной в каждую строку
-            foreach ($newArray as $date) {
-                $line = strval($date);
-                $button = InlineKeyboardButton::make($date, callback_data: "teacher  $line");
-                $keyboard->addRow($button);
-            }
-            $keyboard->addRow(InlineKeyboardButton::make('Другая группа', callback_data: "other_schedule"),
-                InlineKeyboardButton::make('Преподователь', callback_data: "teacher_schedule"),
-                InlineKeyboardButton::make('Твоя группа', callback_data: "other_schedule"));
-            $keyboard->addRow(InlineKeyboardButton::make('Меню', callback_data: "menu_schedule"));
-
-            // Вывод результата
-            $text = implode("\n", $result);
-            return $bot->sendMessage(
-                text: "$text",
-                reply_markup: $keyboard
-            );
-        } else {
-            return $bot->sendMessage('Введи команду /setgroup {parameter} чтобы указать свою группу для бота.');
         }
+
+        $response = $import->client->request('GET', "$date_fromcall/teacher/$teacher");
+        $schedule = (json_decode($response->getBody()->getContents(), true));
+
+        $result[] = "Расписание на $date_fromcall\n $teacher";
+        // Обработка каждого элемента данных
+        foreach ($schedule as $key => $item) {
+            // Проверка наличия необходимых ключей
+            if (isset($item['name'], $item['rooms'])) {
+                // Извлечение необходимых значений
+                $name = str_replace("\n", ' ', $item['name']);  // Замена символов новой строки на пробел
+                $rooms = implode(', ', $item['rooms']);  // Преобразование массива комнат в строку, разделенную запятыми
+
+                // Создание строки с информацией о расписании
+                $scheduleString = "\nНазвание: $name\nАудитория: $rooms";
+
+                // Добавление строки в массив результата
+                $result[] = $scheduleString;
+            } else {
+                $result[] = "Неправильная структура элемента расписания.";
+            }
+        }
+
+        // Создаем объект разметки клавиатуры
+        $keyboard = InlineKeyboardMarkup::make();
+
+        // Добавляем кнопки по одной в каждую строку
+        foreach ($newArray as $date) {
+            $line = strval($date);
+            $button = InlineKeyboardButton::make($date, callback_data: "teacher $line $teacher");
+            $keyboard->addRow($button);
+        }
+        $keyboard->addRow(InlineKeyboardButton::make('Другая группа', callback_data: "other_schedule"),
+            InlineKeyboardButton::make('Преподователь', callback_data: "teacher_schedule"),
+            InlineKeyboardButton::make('Твоя группа', callback_data: "other_schedule"));
+        $keyboard->addRow(InlineKeyboardButton::make('Меню', callback_data: "menu_schedule"));
+
+        // Вывод результата
+        $text = implode("\n", $result);
+        return $bot->sendMessage(
+            text: "$text",
+            reply_markup: $keyboard
+        );
     }
 
     // Метод для обработки команды /start
@@ -209,6 +202,69 @@ class TelegramController extends Controller
             $this->schedule_action($bot);
         }
     }
+
+    public static function schedule_teacher_action(Nutgram $bot, $teacher)
+    {
+        $import = new ApiController();
+        $response = $import->client->request('GET', '');
+        $data = (json_decode($response->getBody()));
+
+        $newArray = [];
+        $counter = 0;
+
+        foreach ($data as $value) {
+            if ($counter < 5) {
+                $date = implode('-', array_reverse(explode('.', $value)));
+                $newArray[] = $date;
+                $counter++;
+            } else {
+                break;
+            }
+        }
+
+        $response = $import->client->request('GET', "$newArray[0]/teacher/$teacher");
+        $schedule = (json_decode($response->getBody()->getContents(), true));
+
+        $result[] = "Расписание на $newArray[0]\nПреподователя $teacher";
+        // Обработка каждого элемента данных
+        foreach ($schedule as $key => $item) {
+            // Проверка наличия необходимых ключей
+            if (isset($item['name'], $item['rooms'])) {
+                // Извлечение необходимых значений
+                $name = str_replace("\n", ' ', $item['name']);  // Замена символов новой строки на пробел
+                $rooms = implode(', ', $item['rooms']);  // Преобразование массива комнат в строку, разделенную запятыми
+
+                // Создание строки с информацией о расписании
+                $scheduleString = "\nНазвание: $name\nАудитория: $rooms";
+
+                // Добавление строки в массив результата
+                $result[] = $scheduleString;
+            } else {
+                $result[] = "Неправильная структура элемента расписания.";
+            }
+        }
+
+        // Создаем объект разметки клавиатуры
+        $keyboard = InlineKeyboardMarkup::make();
+
+        // Добавляем кнопки по одной в каждую строку
+        foreach ($newArray as $date) {
+            $line = strval($date);
+            $button = InlineKeyboardButton::make($date, callback_data: "teacher $line $teacher");
+            $keyboard->addRow($button);
+        }
+        $keyboard->addRow(InlineKeyboardButton::make('Другая группа', callback_data: "other_schedule"),
+            InlineKeyboardButton::make('Преподователь', callback_data: "teacher_schedule"),
+            InlineKeyboardButton::make('Твоя группа', callback_data: "other_schedule"));
+        $keyboard->addRow(InlineKeyboardButton::make('Меню', callback_data: "menu_schedule"));
+
+        // Вывод результата
+        $text = implode("\n", $result);
+        return $bot->sendMessage(
+            text: "$text",
+            reply_markup: $keyboard
+        );
+    }
 }
 
 class TeacherSchedule extends Conversation {
@@ -221,8 +277,9 @@ class TeacherSchedule extends Conversation {
 
     public function secondStep(Nutgram $bot)
     {
-        $Teacher = $bot->message()->text;
-        return TelegramController::schedule_teacher_action($bot, $teacher);
+        $teacher = $bot->message()->text;
+        $bot->sendMessage("Обрабатываю");
         $this->end();
+        TelegramController::schedule_teacher_action($bot, $teacher);
     }
 }
